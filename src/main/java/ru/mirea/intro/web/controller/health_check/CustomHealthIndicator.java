71b6a79dccbf8e2eebc138b26b6fd75538aad2d3
Stass <snide.pancake@gmail.com>
2020-12-25 23:29:30 +0300

    Added: - custom health indicator

---
+++ src/main/java/ru/mirea/intro/web/controller/health_check/CustomHealthIndicator.java
@@ -0,0 +1,14 @@
+package ru.mirea.intro.web.controller.health_check;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator extends AbstractHealthIndicator {
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+       // builder.up();
+    }
+}
